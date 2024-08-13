70ce41770b0868b97571b9d258abe13863b525f9
Ecos1213 <diasflac1213@gmail.com>
2024-08-12 23:11:06 -0500

    Agrega leccion de hola mundo aprendimos a usar la funcion print y tiene como argumento un string sea con comillas simples o dobles y aprendimos a usar comentario con #, ademas aprendimos que python.exe al ejecutarse lo que ahce es tomar nuestro archivo creado HolaMundo.py y el codigo de ejecucion 0 es que todo se ejecuto exitosamente

---
+++ ProyectoInicial/HolaMundo.py
@@ -0,0 +1,7 @@
+# Asi se hace un comentario, con el simbolo #, siempre es recomendado dejar un espacio por cada comentario, es recomandado por python
+# Programa de Hola mundo con python
+print("Hola mundo con Python") # esta funcion esta incluida en el lenguaje de python, esta funcion imprime un mensaje de texto en la terminal o consola, dentro del parentesis proporcionamos la informacion (a esto se le llama parametro)
+# en python podemos usar comilla simple o comilla doble para definir una cadena
+# cuando corremos nuestro programa con click derecho Run, aparece este mensaje Process finished with exit code 0 el codigo 0 significa que todos se ejecuto exitosamente
+# lo que sucede es que el programa de python.exe toma el programa, que hemos creado el archivo, HolaMundo.py (recuerda nuestros archivos de python tendra la extension de .py)
+print('Saludos') # imprimimos con comillas simple, si inicamos con comillas simples terminamos con comillas simples y si usamos comillas dobles terminamos con comillas dobles
